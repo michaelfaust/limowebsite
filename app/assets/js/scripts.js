6ebad77a61d9561498e8b9c5ed2ae984237f9821
fanke371 <fanke371@lu.com>
2016-10-20 17:10:58 +0800

    更改fullpage

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,12 +1,12 @@
 var appMaster = {
 
-    preLoader: function(){
+    preLoader: function() {
         imageSources = []
         $('img').each(function() {
             var sources = $(this).attr('src');
             imageSources.push(sources);
         });
-        if($(imageSources).load()){
+        if ($(imageSources).load()) {
             $('.pre-loader').fadeOut('slow');
         }
     },
@@ -96,11 +96,16 @@ var appMaster = {
     },
 
     animateScript: function() {
-        $('.scrollpoint.sp-effect1').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInLeft');},{offset:'100%'});
-        $('.scrollpoint.sp-effect2').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInRight');},{offset:'100%'});
-        $('.scrollpoint.sp-effect3').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInDown');},{offset:'100%'});
-        $('.scrollpoint.sp-effect4').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeIn');},{offset:'100%'});
-        $('.scrollpoint.sp-effect5').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInUp');},{offset:'100%'});
+        $('.scrollpoint.sp-effect1').waypoint(function() { $(this).toggleClass('active');
+            $(this).toggleClass('animated fadeInLeft'); }, { offset: '100%' });
+        $('.scrollpoint.sp-effect2').waypoint(function() { $(this).toggleClass('active');
+            $(this).toggleClass('animated fadeInRight'); }, { offset: '100%' });
+        $('.scrollpoint.sp-effect3').waypoint(function() { $(this).toggleClass('active');
+            $(this).toggleClass('animated fadeInDown'); }, { offset: '100%' });
+        $('.scrollpoint.sp-effect4').waypoint(function() { $(this).toggleClass('active');
+            $(this).toggleClass('animated fadeIn'); }, { offset: '100%' });
+        $('.scrollpoint.sp-effect5').waypoint(function() { $(this).toggleClass('active');
+            $(this).toggleClass('animated fadeInUp'); }, { offset: '100%' });
     },
 
     revSlider: function() {
@@ -122,12 +127,12 @@ var appMaster = {
         });
     },
 
-    scrollMenu: function(){
+    scrollMenu: function() {
         var num = 50; //number of pixels before modifying styles
         if ($(window).scrollTop() > num) {
             $('nav').addClass('scrolled');
         }
-        $(window).bind('scroll', function () {
+        $(window).bind('scroll', function() {
             if ($(window).scrollTop() > num) {
                 $('nav').addClass('scrolled');
 
@@ -136,14 +141,14 @@ var appMaster = {
             }
         });
 
-        $('ul.navbar-nav li a').bind('click', function(){
-            if($(this).closest('.navbar-collapse').hasClass('in')){
+        $('ul.navbar-nav li a').bind('click', function() {
+            if ($(this).closest('.navbar-collapse').hasClass('in')) {
                 $(this).closest('.navbar-collapse').removeClass('in');
             }
         });
 
     },
-    placeHold: function(){
+    placeHold: function() {
         // run Placeholdem on all elements with placeholders
         Placeholdem(document.querySelectorAll('[placeholder]'));
     }
