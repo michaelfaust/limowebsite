5f845fe0586f8df9e12fa3c6dea0d7b7589c2522
frankfan <frankwaizi2008@gmail.com>
2017-02-16 01:47:14 +0800

    update frontend

--- js/custom.js
+++ js/custom.js
@@ -66,64 +66,11 @@ $(document).ready(function() {
     wow.init();
 
     $('#applyBtn').click(function() {
-        $('.tryform .applyform').addClass('fadeOutUp hide');
-        $('.tryform .applymsg').addClass('fadeInUp show');
+        location.href = "https://fa.fanqier.cn/f/dmfbzl";
     });
 
-    //Newsletter
-    // Checking subcribe form when focus event
-    // $('#newsletter input[type="text"], .assan-newsletter input[type="email"]').live('focus keypress', function() {
-    //     var $email = $(this);
-    //     if ($email.hasClass('error')) {
-    //         $email.val('').removeClass('error');
-    //     }
-    //     if ($email.hasClass('success')) {
-    //         $email.val('').removeClass('success');
-    //     }
-    // });
-    // // Subscribe form when submit to database
-    // $('#newsletter').submit(function() {
-    //     var $CSRF = $(this).find('input[name="_csrf"]');
-    //     var $email = $(this).find('input[name="email"]');
-    //     var $submit = $(this).find('input[name="submit"]');
-    //     var email_pattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
-    //     if (email_pattern.test($email.val()) === false) {
-    //         $email.val('Please enter a valid email address!').addClass('error');
-    //     } else {
-    //         var csrfToken = $CSRF.val();
-    //         $email.attr('disabled', 'disabled');
-    //         $submit.attr('disabled', 'disabled');
-
-    //         var request = $.ajax({
-    //             url: '/signup',
-    //             method: 'POST',
-    //             headers: {
-    //                 'X-CSRF-Token': $CSRF.val()
-    //             },
-    //             data: { email: $email.val() },
-    //             dataType: 'json'
-    //         });
-
-    //         request.done(function(msg) {
-    //             if (parseInt(msg, 0) !== 0) {
-    //                 var msg_split = msg.split('|');
-    //                 if (msg_split[0] === 'success') {
-    //                     $submit.removeAttr('disabled');
-    //                     $email.removeAttr('disabled').val(msg_split[1]).addClass('success');
-    //                 } else {
-    //                     $submit.removeAttr('disabled');
-    //                     $email.removeAttr('disabled').val(msg_split[1]).addClass('error');
-    //                 }
-    //             }
-    //         });
-
-    //         request.fail(function(jqXHR, textStatus) {
-    //             alert("Request failed: " + textStatus);
-    //             $submit.removeAttr('disabled');
-    //             $email.removeAttr('disabled').val('申请成功!').addClass('success');
-    //         });
-    //     }
-
-    //     return false;
-    // });
+    var ts = new Date(2017, 01, 18);
+    $('#countdown').countdown({
+        timestamp: ts,
+    });
 });
